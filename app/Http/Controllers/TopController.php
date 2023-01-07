72229b8170ef1a3f3fe4412911c45306dbb96d37
andrei376 <83941608+andrei376@users.noreply.github.com>
2023-01-07 12:43:43 +0200

    update

--- app/Http/Controllers/TopController.php
+++ app/Http/Controllers/TopController.php
@@ -689,7 +689,7 @@ class TopController extends Controller
         }
 
         $searchField = 'id';
-        $groupField = 'iplong';
+        $groupField = 'iplong,mask';
         $searchValue = '';
         foreach ($request->input('search') as $field => $value) {
             //
