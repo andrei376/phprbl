c111bbafef57c8b0495563b6edd18d112f5dae6a
andrei376 <83941608+andrei376@users.noreply.github.com>
2023-12-09 10:58:38 +0200

    update

--- resources/js/Pages/Rbl/Show6.vue
+++ resources/js/Pages/Rbl/Show6.vue
@@ -225,9 +225,9 @@
                             <elastic :iplong="ipInfo.long2ip" :mask="ipInfo.mask" />
                         </div>
 
-                        <div id="10syslog">
+<!--                        <div id="10syslog">
                             <syslog :iplong="ipInfo.long2ip" :mask="ipInfo.mask" />
-                        </div>
+                        </div>-->
                     </div>
                 </div>
             </div>
