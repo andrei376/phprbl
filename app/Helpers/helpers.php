49fe21c2b218a8f69633a8bf08daef057cb94a1c
Andrei <andrei@mele.newsbox.ro>
2025-08-10 11:53:19 +0300

    update geoip parse

--- app/Helpers/helpers.php
+++ app/Helpers/helpers.php
@@ -23,11 +23,23 @@ function geoip_country_name_by_name($ip)
     $filename = '/var/lib/GeoIP/GeoLite2-City.mmdb';
 
     if (file_exists($filename)) {
-        $cityDbReader = new Reader($filename);
+        try {
+            // Create a new Reader object
+            // This will read the GeoIP database file
+            $cityDbReader = new Reader($filename);
 
-        $record = $cityDbReader->city($ip);
+            $record = $cityDbReader->city($ip);
 
-        return $record->country->name;
+            return $record->country->name;
+        } catch (Exception $e) {
+            Log::error(
+                __METHOD__.
+                ' error reading GeoIP database: '.$e->getMessage().
+                ", line=".$e->getLine().
+                "\n"
+            );
+            return 'GEOIP error';
+        }
     }
 
     return 'TODO GEOIP geoip_country_name_by_name '.__FILE__;
@@ -38,11 +50,25 @@ function geoip_country_code_by_name($ip)
     $filename = '/var/lib/GeoIP/GeoLite2-City.mmdb';
 
     if (file_exists($filename)) {
-        $cityDbReader = new Reader($filename);
+        try {
+            // Create a new Reader object
+            // This will read the GeoIP database file
+            // The Reader class is part of the GeoIP2 library
+            // It allows you to read the GeoIP database and get information about IP addresses
+            $cityDbReader = new Reader($filename);
 
-        $record = $cityDbReader->city($ip);
+            $record = $cityDbReader->city($ip);
 
-        return $record->country->isoCode;
+            return $record->country->isoCode;
+        } catch (Exception $e) {
+            Log::error(
+                __METHOD__.
+                ' error reading GeoIP database: '.$e->getMessage().
+                ", line=".$e->getLine().
+                "\n"
+            );
+            return 'GEOIP error';
+        }        
     }
 
     return 'TODO GEOIP geoip_country_code_by_name '.__FILE__;
